feed00e6d7c3639ca54990557b9dbe5cc75387d5
Vikas <54113274+vikas780@users.noreply.github.com>
2021-02-01 21:52:23 +0530

    Remove my API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAKK1c5LH4oQbiFIi7lZyhIIH5ljPFZ2ZE";
+const KEY = "Add your API key here";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
